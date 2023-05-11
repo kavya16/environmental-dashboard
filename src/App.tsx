d7b9e10899e6cd6f53e0914d24f09ff48ed1e183
Kavya lakshmi Guttikonda <kavyalakshmiguttikonda@Kavyas-MacBook-Air.local>
2023-05-11 17:54:52 -0500

    Added data fetching and chart component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
+
+
 
 function App() {
+  const [environmentalData, setEnvironmentalData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/environmentalData');
+        setEnvironmentalData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Environmental Monitoring Dashboard</h1>
+      <ChartComponent data={environmentalData} />
     </div>
-  );
+  );  
 }
 
 export default App;
+
+
+
+
+
+
+
+
+const [environmentalData, setEnvironmentalData] = useState([]);
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('/api/environmental-data'); // Replace with your actual API endpoint
+      setEnvironmentalData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  fetchData();
+}, []);
+
+
+
+
+type ChartComponentProps = {
+  data: any[]; // Update this with the appropriate data type
+};
+
+const ChartComponent: React.FC<ChartComponentProps> = ({ data }) => {
+  // Prepare data for the chart
+  const chartData = {
+    labels: data.map((item) => item.label), // Update this based on your data structure
+    datasets: [
+      {
+        label: 'Environmental Data', // Update with your desired label
+        data: data.map((item) => item.value), // Update this based on your data structure
+        backgroundColor: 'rgba(75, 192, 192, 0.6)', // Set desired background color
+        borderColor: 'rgba(75, 192, 192, 1)', // Set desired border color
+        borderWidth: 1, // Set desired border width
+      },
+    ],
+  };
+
+  // Configure chart options
+  const chartOptions = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <Bar data={chartData} options={chartOptions} />;
+};
